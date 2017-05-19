baffdcc75be5f3768c36763942d01a44c29b7fce
ellun <eltoncheng86@gmail.com>
2017-05-19 14:21:18 -0400

    Running server with gulp

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # angular-email
 Building an email clone using angularjs
+
+```
+npm init
+npm install jquery --save
+npm install angular@1.5.6 --save
+```
+
+### Required Tools
+jQuery - Shorthand framework for Vanilla JavaScript. By default Angular uses jQLite, we will be loading jQuery before Angular to have Angular use jQuery instead.
+```
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+```
+
+AngularJS - Frontend framework
+```
+<script src="/node_modules/angular/angular.js"></script>
+```
---
+++ gulpfile.babel.js
@@ -0,0 +1,63 @@
+'use strict';
+
+import gulp from 'gulp';
+import sass from 'gulp-sass';
+import autoprefixer from 'gulp-autoprefixer';
+import connect from 'gulp-connect';
+const autoprefixerOptions = {
+  browsers: [
+    "Android 2.3",
+    "Android >= 4",
+    "Chrome >= 20",
+    "Firefox >= 24",
+    "Explorer >= 8",
+    "iOS >= 6",
+    "Opera >= 12",
+    "Safari >= 6"
+  ]
+};
+
+
+
+// Basic pattern:
+//
+//  gulp.task('task-name', function() {
+//    // Stuff here
+//  });
+
+// Applying a plugin to a directory:
+//
+//  gulp.task('task-name', function () {
+//    return gulp.src('source-files') // Get source files with gulp.src
+//      .pipe(aGulpPlugin()) // Sends it through a gulp plugin
+//      .pipe(gulp.dest('destination')) // Outputs the file in the destination folder
+//  })
+
+// Watching a directory:
+//
+//  gulp.watch('files-to-watch', ['tasks', 'to', 'run']);
+
+
+
+gulp.task('default', function() {
+  console.log('Hello!');
+});
+
+gulp.task('sass', function() {
+  return gulp
+    .src('app/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('watch', function(){
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: '.',
+    livereload: true
+  })
+});
